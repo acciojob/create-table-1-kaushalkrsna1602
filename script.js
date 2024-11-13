9fd1507f4d391d52f6ba033108696b1f71fca08e
Kaushal Kumar <Kaushal.kr.kr@gmail.com>
2024-11-13 20:06:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	const table = document.querySelector(#sampleTable);
+
+	let newRow = table.insertRow(0);
+
+	let cell1 = newRow.insertCell(0);
+	let cell2 = newRow.insertCCell(1);
+
+	cell1.innerHTML = "New Cell1"
+	cell2.innerHTML = "New Cell2"
 }
